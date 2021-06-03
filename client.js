0ab19e4024dfc561600355e7b91525c77d8589a2
Warren Brown <warrengbrn@gmail.com>
2021-06-03 14:55:23 -0400

    added function to log player count selected by client, needs some touching up

--- client.js
+++ client.js
@@ -17,9 +17,31 @@ window.onclick = function(event){
 }
 
 //sends selected player count to server
-document.getElementById("myDropdown").addEventListener("click", function(){
-  console.log(document.getElementById("myDropdown").innerHTML);
-})
+
+document.getElementById('p1').addEventListener("click", function(){
+  console.log(document.getElementById('p1').innerHTML);
+});
+document.getElementById('p2').addEventListener("click", function(){
+  console.log(document.getElementById('p2').innerHTML);
+});
+document.getElementById('p3').addEventListener("click", function(){
+  console.log(document.getElementById('p3').innerHTML);
+});
+document.getElementById('p4').addEventListener("click", function(){
+  console.log(document.getElementById('p4').innerHTML);
+});
+document.getElementById('p5').addEventListener("click", function(){
+  console.log(document.getElementById('p5').innerHTML);
+});
+document.getElementById('p6').addEventListener("click", function(){
+  console.log(document.getElementById('p6').innerHTML);
+});
+document.getElementById('p7').addEventListener("click", function(){
+  console.log(document.getElementById('p7').innerHTML);
+});
+document.getElementById('p8').addEventListener("click", function(){
+  console.log(document.getElementById('p8').innerHTML);
+});
 
 
 
