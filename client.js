8a188aedb843892e685071edfe1274c85ac30eb0
Warren Brown <warrengbrn@gmail.com>
2021-06-03 20:52:02 -0400

    simplified player count selector of front end

--- client.js
+++ client.js
@@ -17,34 +17,12 @@ window.onclick = function(event){
 }
 
 //sends selected player count to server
-
-document.getElementById('p1').addEventListener("click", function(){
-  console.log(document.getElementById('p1').innerHTML);
-});
-document.getElementById('p2').addEventListener("click", function(){
-  console.log(document.getElementById('p2').innerHTML);
-});
-document.getElementById('p3').addEventListener("click", function(){
-  console.log(document.getElementById('p3').innerHTML);
-});
-document.getElementById('p4').addEventListener("click", function(){
-  console.log(document.getElementById('p4').innerHTML);
-});
-document.getElementById('p5').addEventListener("click", function(){
-  console.log(document.getElementById('p5').innerHTML);
-});
-document.getElementById('p6').addEventListener("click", function(){
-  console.log(document.getElementById('p6').innerHTML);
-});
-document.getElementById('p7').addEventListener("click", function(){
-  console.log(document.getElementById('p7').innerHTML);
-});
-document.getElementById('p8').addEventListener("click", function(){
-  console.log(document.getElementById('p8').innerHTML);
-});
-
-
-
+let x = document.querySelectorAll('a');
+for (i of x) {
+  i.addEventListener('click', function(){
+    console.log(this.id);
+  })
+}
 
 
 const socket = new WebSocket('ws://localhost:8080'); 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const WebSocket = require('ws');
 const ws = new WebSocket.Server({ port: 8080 });
 
+console.log("server.js started")
+
 ws.on('connection', function connection(wsConnection) {
   wsConnection.on('message', function incoming(message) {
     console.log(`server received: ${message}`);
