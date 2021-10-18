afa77dc553c8dc23606260c414ccffa29b76c6c2
Warren Brown <warrengbrn@gmail.com>
2021-10-18 10:04:00 -0400

    cleaned up repetitive functions, removed abrupt transition to white

--- Flexbox2/functions.js
+++ Flexbox2/functions.js
@@ -1,49 +1,36 @@
-var x = 0;
-var y = 0;
+function animateHelp(value){
+    var x;
+    var y;
+    if(value == "help"){
+        x = "Help";
+        y = "Settings";
+    }else{
+        x = "Settings";
+        y = "Help";
+    }
+    var a = document.getElementById(`dropdown${x}`);
+    var b = document.getElementById(`dropdown${y}`);
 
-function animateHelp(){
+        if(a.className == ''){
+            //show and fade in
+            a.style.animation = "grow 0s ease-in-out forwards";
+            a.className = 'fade'; 
     
-    if(x == 0){
-        //show and fade in
-        document.getElementById('dropdownHelp').style.animation = "grow 0s ease-in-out forwards";
-        document.getElementById('dropdownHelp').className = 'fade'; 
-        x = 1;
-
-        if(y == 1){
-            //hide competing dropdown and fade out
-            document.getElementById('dropdownSettings').style.animation = "shrink 0s ease-in-out forwards";
-            document.getElementById('dropdownSettings').className = ''; 
-            y = 0;
+            if(b.className = 'fade'){
+                //hide competing dropdown and fade out
+                b.style.animation = "shrink 0s ease-in-out forwards";
+                b.className = ''; 
+                //keep background color the same during transition
+                document.body.style.backgroundColor = "rgb(253, 68, 68)";
+            }
+        }else{
+            //fade out then hide
+            a.className = '';
+            setTimeout(function() {
+                a.style.animation = "shrink 0s ease-in-out forwards";
+            }, 500);
+            //return background color to white if changed
+            document.body.style.backgroundColor = "white";
         }
-    }else{
-        //hide and fade out
-        document.getElementById('dropdownHelp').className = '';
-        setTimeout(function() {
-            document.getElementById('dropdownHelp').style.animation = "shrink 0s ease-in-out forwards";
-        }, 500);
-        x = 0;
-    }
-}
 
-function animateSettings(){
-    if(y == 0){
-        //show and fade in
-        document.getElementById('dropdownSettings').style.animation = "grow 0s ease-in-out forwards";
-        document.getElementById('dropdownSettings').className = 'fade';
-        y = 1;
-
-        if(x == 1){
-            //hide competing dropdown and fade out
-            document.getElementById('dropdownHelp').style.animation = "shrink 0s ease-in-out forwards";
-            document.getElementById('dropdownHelp').className = ''; 
-            x = 0;
-        }
-    }else{
-        //hide and fade out
-        document.getElementById('dropdownSettings').className = '';
-        setTimeout(function() {
-            document.getElementById('dropdownSettings').style.animation = "shrink 0s ease-in-out forwards";
-        }, 500);
-        y = 0;
-    }
 }
