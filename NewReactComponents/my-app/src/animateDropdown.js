798ef81f66eba80fc8ac7140ddea54de51ded003
Warren Brown <warrengbrn@gmail.com>
2021-10-25 16:18:45 -0400

    simplified animation dropdown syntax

--- NewReactComponents/my-app/src/animateDropdown.js
+++ NewReactComponents/my-app/src/animateDropdown.js
@@ -1,33 +1,31 @@
-var x;
-var y;
+var a;
+var b;
 
 export const animateDropdown = e => {
     if(e.target.id == 'help'){
-        x = "Help";
-        y = "Settings";
+        a = document.getElementById('dropdownHelp');
+        b = document.getElementById('dropdownSettings');
     }else{
-        x = "Settings";
-        y = "Help";
+        a = document.getElementById('dropdownSettings');
+        b = document.getElementById('dropdownHelp');
     }
-    var a = document.getElementById(`dropdown${x}`);
-    var b = document.getElementById(`dropdown${y}`);
     var c = document.getElementById("mainGrid");
-        if(a.style.zIndex == 0){
-            //swap transparencies and z-index
-            a.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 200, fill: 'forwards' });
-            c.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 200, fill: 'forwards' });
-            setTimeout(() => { a.style.zIndex = 1; c.style.zIndex = 0; }, 200);
-    
-            if(b.style.zIndex == 1){
-                //transparent background and last clicked dropdown, send last clicked to bottom
-                b.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 200, fill: 'forwards' });
-                c.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 0, fill: 'forwards' });
-                setTimeout(() => { b.style.zIndex = 0; }, 200);
-            }
-        }else{
-            //swap transparencies and z-index back to original
-            a.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 200, fill: 'forwards' });
-            c.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 200, fill: 'forwards' });
-            setTimeout(() => { a.style.zIndex = 0; c.style.zIndex = 1; }, 200);
+    if(a.style.zIndex == 0){
+        //swap transparencies and z-index
+        a.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 200, fill: 'forwards' });
+        c.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 200, fill: 'forwards' });
+        setTimeout(() => { a.style.zIndex = 1; c.style.zIndex = 0; }, 200);
+
+        if(b.style.zIndex == 1){
+            //transparent background and last clicked dropdown, send last clicked to bottom
+            b.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 200, fill: 'forwards' });
+            c.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 0, fill: 'forwards' });
+            setTimeout(() => { b.style.zIndex = 0; }, 200);
         }
+    }else{
+        //swap transparencies and z-index back to original
+        a.animate([{ opacity: 1 }, { opacity: 0 }], { duration: 200, fill: 'forwards' });
+        c.animate([{ opacity: 0 }, { opacity: 1 }], { duration: 200, fill: 'forwards' });
+        setTimeout(() => { a.style.zIndex = 0; c.style.zIndex = 1; }, 200);
+    }
 }
