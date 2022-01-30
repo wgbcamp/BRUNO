9a95cb6bb0135ef0a0f57c4eb7ff02ef3b60a490
Warren Brown <warrengbrn@gmail.com>
2022-01-29 23:31:47 -0500

    changes to blurring, removal of dimming function

--- NewReactComponents/client/src/pages/Main.js
+++ NewReactComponents/client/src/pages/Main.js
@@ -22,23 +22,24 @@ function App() {
     })
 
     const [playerCount, setPlayers] = useState(2);
-    const [showPopup, switchPopup] = useState("false");
-    const [showDim, switchDim] = useState(false);
+    const [showPopup, switchPopup] = useState(false);
+    const [showBlur, switchBlur] = useState(false);
 
     const changeStyle = () => {
         switchPopup(!showPopup);
     }
 
-    const dim = (data) => {  
+    const blur = (data) => {  
         console.log(data);
         if(data == true){
-            switchDim(true);
+            switchBlur(true);
         }else{
-            switchDim(false);
+            switchBlur(false);
         }
     }
 
-    const [darkMode, switchDarkMode] = useState(true);
+    //dark mode switch
+    const [darkMode, switchDarkMode] = useState(false);
     const [dModeHTML, switchHTML] = useState(darkMode ? "Disable Dark Mode" : "Enable Dark Mode");
 
     const changeDarkMode = () => {
@@ -64,38 +65,42 @@ function App() {
         API.createSession(playerCount);
     }
 
+    const submitStyle = {
+        backgroundColor: darkMode ? "#4cdd81" : "#4cdd81",
+        color: darkMode ? "white" : "white"
+    }
+
   return (
-    <div id="container" className="container">  
-    <HeaderBar darkMode={darkMode} showPopup={showPopup} dimFunction={dim}/>   
-    <div id="mainGrid" className={`${showPopup ? "mainGrid1" : "mainGrid2"} ${showDim ? "blur" : "no-blur"}`} style={{backgroundColor: darkMode ? "#3d298a" : "white"}}>
-        <Help />
+    <div id="container" className={darkMode ? "container1" : "container2"}>  
+    <HeaderBar darkMode={darkMode} showPopup={showPopup} blurFunction={blur}/>   
+    <div id="mainGrid" className={`${"mainGrid1"} ${showBlur ? "blur" : "no-blur"}`} style={{backgroundColor: darkMode ? "#3d298a" : "white"}}>
         <div id="title" className="title" style={{color: darkMode ? "white" : "black"}}> 
             BRUNO
             <div id="subtitle" className="subtitle" style={{color: darkMode ? "#ededed" : "#383838"}}>
                 A recreation of the classic crazy eights card game.
             </div>
         </div>
-        <div id="createSession" className="createSession" onClick={changeStyle}>
+        <div id="createSession" className="createSession" onClick={() => {changeStyle(); switchBlur(!showBlur)}} style={{backgroundColor: darkMode ? "#c079ff" : "#3d298a"}}>
             Create Session
         </div>
-        <div id="publicSession" className="publicSession">
+        <div id="publicSession" className="publicSession" style={{backgroundColor: darkMode ? "#c079ff" : "#3d298a"}}>
             Browse Public Games
         </div>
-        <div id="searchForSession" className="searchForSession">
+        <div id="searchForSession" className="searchForSession" style={{backgroundColor: darkMode ? "#c079ff" : "#3d298a"}}>
             Search for Private Game
         </div>
     </div>
-    <div id="sessionPopupContainer" className={showPopup ? "sessionPopupContainer1" : "sessionPopupContainer2"} onClick={changeStyle}>
-        <div id="sessionPopup" className={showPopup ? "sessionPopup1" : "sessionPopup2"} onClick={(e) => e.stopPropagation()}>
+    <div id="sessionPopupContainer" className={showPopup ? "sessionPopupContainer1" : "sessionPopupContainer2"} onClick={() => {changeStyle(); switchBlur(!showBlur)}}>
+        <div id="sessionPopup" className={showPopup ? "sessionPopup1" : "sessionPopup2"} style={{backgroundColor: darkMode? "#c079ff" : "#3d298a"}} onClick={(e) => e.stopPropagation()}>
             <div id="playerCountText" className="playerCountText">
                 How many players?
             </div>
             <div id="playerCountContainer" className="playerCountContainer">
-            <div id="arrow" className="arrowLeft" onClick={() => decrement()}><i className="fas fa-long-arrow-alt-left"></i></div>
-            <div id="playerCount" className="playerCount">{playerCount}</div>
-            <div id="arrow" className="arrowRight" onClick={() => increment()}><i className="fas fa-long-arrow-alt-right"></i></div>
+            <div id="arrow" className="arrowLeft" style={{backgroundColor: darkMode ? "#d7aaff" : "#706bff"}} onClick={() => decrement()}><i className="fas fa-long-arrow-alt-left"></i></div>
+            <div id="playerCount" className="playerCount" style={{backgroundColor: darkMode ? "#d09aff" : "#5c57e6"}}>{playerCount}</div>
+            <div id="arrow" className="arrowRight" style={{backgroundColor: darkMode ? "#d7aaff" : "#706bff"}} onClick={() => increment()}><i className="fas fa-long-arrow-alt-right"></i></div>
             </div>
-            <div id="confirm" className="confirm" onClick={submitCode}>
+            <div id="confirm" className="confirm" style={submitStyle} onClick={submitCode}>
                 Submit
             </div>
         </div>
--- NewReactComponents/client/src/subComponents/headerBar.js
+++ NewReactComponents/client/src/subComponents/headerBar.js
@@ -5,43 +5,59 @@ function HeaderBar(props){
 
     const [helpStatus, toggleHelp] = useState(false);
     const [settingsStatus, toggleStatus] = useState(false);
-    const [dimStatus, toggleDim] = useState(false);
+    const [blurStatus, toggleBlur] = useState(false);
 
     const helpDropdown = () => {
-        toggleStatus(false);
-        toggleHelp(!helpStatus);  
+        if(settingsStatus == true){
+            toggleStatus(false);
+            toggleHelp(!helpStatus);
+        }else{
+            toggleHelp(!helpStatus);
+            blurMainGrid();
+        } 
+
     }
     const settingsDropdown = () => {
-        toggleHelp(false);
-        toggleStatus(!settingsStatus);
+        if(helpStatus == true){
+            toggleHelp(false);
+            toggleStatus(!settingsStatus);
+        }else{
+            toggleStatus(!settingsStatus);
+            blurMainGrid();
+        }       
     }
 
-    const dimMainGrid = () => {
-        toggleDim(!dimStatus);
-        if(dimStatus == false){
-            props.dimFunction(true);
+    const blurMainGrid = () => {
+        toggleBlur(!blurStatus);
+        if(blurStatus == false){
+            props.blurFunction(true);
         }else{
-            props.dimFunction(false);
+            props.blurFunction(false);
         }     
+    }
+
+    const headerBarStyle = {
+        backgroundColor: props.darkMode ? "#d549eb" : "#fdf906",
+        color: props.darkMode ? "white" : "black"
     }
 
     return(
         <div>
-            <div id="headerBar" className={props.showPopup ? "headerBar1" : "headerBar2"} style={{backgroundColor: props.darkMode ? '#d549eb' : '#fdf906'}}>
+            <div id="headerBar" className={props.showPopup ? "headerBar1" : "headerBar2"} style={headerBarStyle}>
                 <div id="siteLogo" className="siteLogo">
                     <div id="placeholderLogo" className="placeholderLogo">
                     BRUNO
                     </div>
                 </div>
-                <div id="help" className="help" style={{color: 'black'}} onClick={() =>{helpDropdown(); dimMainGrid();}}>Help </div>
+                <div id="help" className="help" onClick={() =>{helpDropdown();}}>Help </div>
 
-                <div id="settings" className="settings" style={{color: 'black'}} onClick={settingsDropdown}>Settings</div>
+                <div id="settings" className="settings" onClick={() => {settingsDropdown();}}>Settings</div>
 
-                <div id="source" className="source"><a href="https://github.com/wgbcamp/BRUNO"><i className="fab fa-github" style={{color: 'black'}}> </i></a>
+                <div id="source" className="source"><a href="https://github.com/wgbcamp/BRUNO"><i className="fab fa-github" style={headerBarStyle}> </i></a>
                 </div>
             </div>
             <div className="helpContainer">
-                <div className={helpStatus ? "dropdownHelp2" : "dropdownHelp1"} style={{backgroundColor: props.darkMode ? "#d549eb" : "white", color: props.darkMode ? "black" : "#ededed"}}>
+                <div className={helpStatus ? "dropdownHelp2" : "dropdownHelp1"} style={{backgroundColor: props.darkMode ? "#d549eb" : "#3d298a", color: props.darkMode ? "white" : "#ededed"}}>
             <p>The key to winning a game of <u><b>BRUNO</b></u>, is by being the first player to play all of the cards in your hand each round.</p>
 
                             <p>The <u>first player</u> with no cards left in hand is awarded <u>points</u> based on the cards that are still left in each of their opponents' hands.</p>
@@ -75,10 +91,10 @@ function HeaderBar(props){
             </div>
             <div className="settingsContainer">
                 <div className={settingsStatus ? "dropdownSettings2" : "dropdownSettings1"}>
-                    <div className="volume">Sounds: ON/OFF</div>
-                    <div className="textSize">Change text size</div>
-                    <div className="darkMode">placeholder for dark mode</div>
-                    <div className="leaveSession">Leave Session</div>
+                    <div className="volume" style={{backgroundColor: props.darkMode ? "#c079ff" : "#3d298a"}}>Sounds: ON/OFF</div>
+                    <div className="textSize" style={{backgroundColor: props.darkMode ? "#c079ff" : "#3d298a"}}>Change text size</div>
+                    <div className="darkMode" style={{backgroundColor: props.darkMode ? "#c079ff" : "#3d298a"}}>placeholder for dark mode</div>
+                    <div className="leaveSession" style={{backgroundColor: props.darkMode ? "#c079ff" : "#3d298a"}}>Leave Session</div>
                 </div>
             </div>
     </div>
